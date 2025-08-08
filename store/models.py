dc7e25d47412d39e9f6b66e632e4df37f566381e
Mohamad Fouad <mamofouad505@gmail.com>
2025-08-08 15:13:07 +0300

    mod

--- store/models.py
+++ store/models.py
@@ -1,3 +1,81 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    raiting = models.DecimalField(max_digits=3, decimal_places=2, default=0.0)
+
+    def __str__(self):
+        return self.name
+    
+class CustomDesign(models.Model):
+    customer_name = models.CharField(max_length=100)
+    customer_email = models.EmailField()
+    uploaded_design = models.FileField(upload_to="custom_designs/")
+    notes = models.TextField(blank=True)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Custom Design by {self.customer_name}"
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart {self.id}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name}"
+    
+class Order(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, default='Pending')
+    
+    def __str__(self):
+        return self.status
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return f"{self.quantity} of {self.product.name} in Order {self.order.id}"
+    
+class whiclist(models.Model):
+    user = models.CharField(max_length=100)
+    products = models.ManyToManyField(Product, blank=True)
+
+    def __str__(self):
+        return f"Wishlist of {self.user}"
+    
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.CharField(max_length=100)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Review by {self.user} for {self.product.name} - Rating: {self.rating}"
+
